c34de8c9c9b7c60e56614fe2015ca2ed56c6a157
Yair Sadan <122673750+YairSadan@users.noreply.github.com>
2023-06-05 15:03:38 +0300

    Add files via upload

---
+++ emailSMTP.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  document
+    .getElementById("contact-form2")
+    .addEventListener("submit", sendEmail);
+  document.getElementById("contact-form").addEventListener("submit", sendEmail);
+};
+
+function sendEmail(event) {
+  event.preventDefault();
+  // these IDs from the previous steps
+  emailjs.sendForm("service_vzdy9yj", "template_xety1he", this).then(
+    function () {
+      console.log("SUCCESS!");
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+}
