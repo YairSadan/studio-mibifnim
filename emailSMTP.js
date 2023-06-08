e81b08267e97b6048966cf4e9c8ad149337715bb
Yair Sadan <yairsadan1@gmail.com>
2023-06-08 15:49:33 +0300

    update

--- emailSMTP.js
+++ emailSMTP.js
@@ -1,21 +1,20 @@
 window.onload = function () {
-  const form = document.getElementById("contact-form2");
-  form.addEventListener("submit", function(event){sendEmail(event, form)});
-  const bottomForm = document.getElementById("contact-form")
-  bottomForm.addEventListener("submit", function(event){sendEmail(event, bottomForm)});
+  document
+    .getElementById('contact-form2')
+    .addEventListener('submit', sendEmail);
+  document.getElementById('contact-form').addEventListener('submit', sendEmail);
 };
 
-function sendEmail(event, form) {
+function sendEmail(event) {
   event.preventDefault();
   // these IDs from the previous steps
-  emailjs.sendForm("service_vzdy9yj", "template_xety1he", this).then(
+  emailjs.sendForm('service_vzdy9yj', 'template_xety1he', this).then(
     function () {
-      form.reset();
-      window.alert("הפרטים נשלחו בהצלחה!");
-      console.log("SUCCESS!");
+      window.alert('הפרטים נשלחו בהצלחה!');
+      console.log('SUCCESS!');
     },
     function (error) {
-      console.log("FAILED...", error);
+      console.log('FAILED...', error);
     }
   );
 }
